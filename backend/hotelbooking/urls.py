c3e7ed6e1cc0987872d7362405c34f073711d3eb
med-sidmed <medsid2380@gmail.com>
2026-02-15 11:16:27 +0000

    distribute tasks ,creating two apps

--- backend/hotelbooking/urls.py
+++ backend/hotelbooking/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('hotelAPI.urls')),
+    path('api/', include('accounts.urls')),
+    path('api/', include('hotel.urls')),
 ]
